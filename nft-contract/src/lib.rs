32fca1978b7af6b5d49d5cdf2b5a3589d99f06d7
Ricky <zradelaide1122@163.com>
2022-05-03 02:02:07 +0800

    第一部分mint

--- nft-contract/src/lib.rs
+++ nft-contract/src/lib.rs
@@ -7,12 +7,14 @@ use near_sdk::{
     env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise, PromiseOrValue,
 };
 
+use crate::internal::*;
 pub use crate::metadata::*;
 pub use crate::mint::*;
 pub use crate::nft_core::*;
 pub use crate::approval::*;
 pub use crate::royalty::*;
 
+mod internal;
 mod approval; 
 mod enumeration; 
 mod metadata; 
@@ -20,12 +22,30 @@ mod mint;
 mod nft_core; 
 mod royalty; 
 
+/// 创建一个公共可见的合约
+/// 通过derive 给我们的Contract 实现了 3个trait  BorshDeserialize  BorshSerialize PanicOnDefault
+///
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
-    /*
-        FILL THIS IN
-    */
+    //contract owner
+    pub owner_id: AccountId,
+
+    //keeps track of all the token IDs for a given account
+    // near_sdk_rs/src/collections/lookup_map 一个map 实现 通过账户id 获取到该账户下所有代币信息
+    pub tokens_per_owner: LookupMap<AccountId, UnorderedSet<TokenId>>,
+
+    //keeps track of the token struct for a given token ID
+    // 通过代币id 查询代币内部信息
+    pub tokens_by_id: LookupMap<TokenId, Token>,
+
+    //keeps track of the token metadata for a given token ID
+    // 通过代币id获取到代币元数据信息
+    pub token_metadata_by_id: UnorderedMap<TokenId, TokenMetadata>,
+
+    //keeps track of the metadata for the contract
+    pub metadata: LazyOption<NFTContractMetadata>,
 }
 
 /// Helper structure for keys of the persistent collections.
@@ -50,10 +70,19 @@ impl Contract {
     */
     #[init]
     pub fn new_default_meta(owner_id: AccountId) -> Self {
-        /*
-            FILL THIS IN
-        */
-        todo!(); //remove once code is filled in.
+        //calls the other function "new: with some default metadata and the owner_id passed in 
+        Self::new(
+            owner_id,
+            NFTContractMetadata {
+                spec: "May3rdTesting".to_string(),
+                name: "MySecondNft".to_string(),
+                symbol: "LetsGoPPMTCLOUD".to_string(),
+                icon: None,
+                base_uri: None,
+                reference: None,
+                reference_hash: None,
+            },
+        )
     }
 
     /*
@@ -63,9 +92,23 @@ impl Contract {
     */
     #[init]
     pub fn new(owner_id: AccountId, metadata: NFTContractMetadata) -> Self {
-        /*
-            FILL THIS IN
-        */
-        todo!(); //remove once code is filled in.
+        //create a variable of type Self with all the fields initialized. 
+        let this = Self {
+            //Storage keys are simply the prefixes used for the collections. This helps avoid data collision
+            tokens_per_owner: LookupMap::new(StorageKey::TokensPerOwner.try_to_vec().unwrap()),
+            tokens_by_id: LookupMap::new(StorageKey::TokensById.try_to_vec().unwrap()),
+            token_metadata_by_id: UnorderedMap::new(
+                StorageKey::TokenMetadataById.try_to_vec().unwrap(),
+            ),
+            //set the owner_id field equal to the passed in owner_id. 
+            owner_id,
+            metadata: LazyOption::new(
+                StorageKey::NFTContractMetadata.try_to_vec().unwrap(),
+                Some(&metadata),
+            ),
+        };
+    
+        //return the Contract object
+        this
     }
 }
